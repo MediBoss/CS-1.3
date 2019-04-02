a33df8237eada812c2247af33aec16d53e1df932
Medi Assumani <mediassumani49@gmail.com>
2019-04-02 11:15:30 -0700

    [Add] implementation for decode for base 2,16, and 36

--- source/Base/bases.py
+++ source/Base/bases.py
@@ -10,20 +10,29 @@ import string
 # string.ascii_letters is ascii_lowercase + ascii_uppercase
 # string.printable is digits + ascii_letters + punctuation + whitespace
 
+BASE_36 = {"A": 10, "B": 11,"C": 12, "D": 13, "E": 14, "F": 15, "G": 16, "H": 17,"I": 18, "J": 19, "K": 20, "L": 21, "M": 22, "N": 23, "O": 24, "P": 25, "Q": 26, "R": 27, "S": 28, "T": 29,"U": 30, "V": 31, "W": 32, "X": 33, "Y":34, "Z": 35}
 
 def decode(digits, base):
-    """Decode given digits in given base to number in base 10.
-    digits: str -- string representation of number (in given base)
-    base: int -- base of given number
-    return: int -- integer representation of number (in base 10)"""
-    # Handle up to base 36 [0-9a-z]
+    """
+        Decode given digits in given base to number in base 10.
+        - Parameters: 
+            - digits: str -- string representation of number (in given base)
+            -base: int -- base of given number
+        Returns: int -- integer representation of number (in base 10)
+        WorstCase: 
+    """
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
-    # TODO: Decode digits from binary (base 2)
-    # ...
-    # TODO: Decode digits from hexadecimal (base 16)
-    # ...
-    # TODO: Decode digits from any base (2 up to 36)
-    # ...
+    length = len(digits)
+    converted_digits = 0
+
+    for digit in digits:
+        length -= 1
+        if digit in BASE_36:
+            digit = BASE_36.get(digit)
+
+        converted_digits += (int(digit) * pow(base, length))
+    return converted_digits
+    
 
 
 def encode(number, base):
@@ -64,18 +73,21 @@ def convert(digits, base1, base2):
 
 def main():
     """Read command-line arguments and convert given digits between bases."""
-    import sys
-    args = sys.argv[1:]  # Ignore script file name
-    if len(args) == 3:
-        digits = args[0]
-        base1 = int(args[1])
-        base2 = int(args[2])
-        # Convert given digits between bases
-        result = convert(digits, base1, base2)
-        print('{} in base {} is {} in base {}'.format(digits, base1, result, base2))
-    else:
-        print('Usage: {} digits base1 base2'.format(sys.argv[0]))
-        print('Converts digits from base1 to base2')
+
+    digits = "7Z"
+    print(decode(digits, 36))
+    # import sys
+    # args = sys.argv[1:]  # Ignore script file name
+    # if len(args) == 3:
+    #     digits = args[0]
+    #     base1 = int(args[1])
+    #     base2 = int(args[2])
+    #     # Convert given digits between bases
+    #     result = convert(digits, base1, base2)
+    #     print('{} in base {} is {} in base {}'.format(digits, base1, result, base2))
+    # else:
+    #     print('Usage: {} digits base1 base2'.format(sys.argv[0]))
+    #     print('Converts digits from base1 to base2')
 
 
 if __name__ == '__main__':
