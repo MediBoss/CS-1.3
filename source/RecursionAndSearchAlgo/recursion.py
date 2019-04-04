c27ea024e2ef275c2b48ac8c46133a2db4bc0e62
Medi Assumani <mediassumani49@gmail.com>
2019-04-03 18:19:29 -0700

    [Add] completed factorial iterative 100% test coverage

--- source/RecursionAndSearchAlgo/recursion.py
+++ source/RecursionAndSearchAlgo/recursion.py
@@ -8,16 +8,18 @@ def factorial(n):
         raise ValueError('factorial is undefined for n = {}'.format(n))
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    # return factorial_iterative(n)
-    return factorial_recursive(n)
+    return factorial_iterative(n)
+    #return factorial_recursive(n)
 
 
 def factorial_iterative(n):
-    # TODO: implement the factorial function iteratively here
-    pass
-    # once implemented, change factorial (above) to call factorial_iterative
-    # to verify that your iterative implementation passes all tests
 
+    result = 1
+    while n != 0:
+        result *= n
+        n -= 1
+    
+    return result
 
 def factorial_recursive(n):
     # check if n is one of the base cases
@@ -30,6 +32,7 @@ def factorial_recursive(n):
 
 
 def main():
+
     import sys
     args = sys.argv[1:]  # Ignore script file name
     if len(args) == 1:
