0fc39229f798ab56ff968884cc908c9f09ff12ea
Medi Assumani <mediassumani49@gmail.com>
2019-04-15 18:29:36 -0700

    [Add] Stack methods with 100% test coverage

--- source/stack.py
+++ source/stack.py
@@ -1,6 +1,6 @@
 #!python
 
-from  source.ArrayAndLinkedList.linkedlist import LinkedList
+from  linkedlist import LinkedList
 
 
 # Implement LinkedStack below, then change the assignment at the bottom
@@ -21,27 +21,52 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this stack."""
         # TODO: Count number of items
+        return self.list.length()
 
     def push(self, item):
         """Insert the given item on the top of this stack.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Push given item
+        Running time: O(1) – Since the LinkedList's prepend fnution is constant time"""
 
+        self.list.prepend(item)
+        
     def peek(self):
         """Return the item on the top of this stack without removing it,
-        or None if this stack is empty."""
-        # TODO: Return top item, if any
+        or None if this stack is empty.
+        Running Time: O(1) since we are just grabbing the head's data
+        """
+
+        if self.list.is_empty() is True:
+            return None
+        
+        return self.list.head.data
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        Running time: O(1) – Since getting the head from the the ll is constant time"""
+        
+        # Raises an error if trying to pop from an empty stack
+        if self.list.is_empty() is True:
+            raise ValueError("Cannot pop element from empty stack")
+
+        else:
+            # grab the item that must be returned from the top/head
+            target_item = self.list.head.data
+            if self.list.head:
+                # Unlink and Shift the top/head one node down on the stack
+                self.list.head = self.list.head.next
+            else:
+                # Nullify the top/head since we've already gotten its value
+                self.list.head = None
+
+        # Update the size of the stack and return the item from the top
+        self.list.size -= 1
+        return target_item
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
